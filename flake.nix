8c1d0d536e09476d4f3de207f30c2f1fb40c65a6
Louis-Mael Derval <louis-mael.derval@epita.fr>
2025-07-05 17:52:53 +0200

    chore(runtime): create a C runtime for the language

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A flake for the Compiler Playground";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
@@ -41,8 +41,29 @@
               cp $out/bin/main.exe $out/bin/${self.packages.${system}.playground.pname}
             '';
           };
-          default = self.packages.${system}.playground;
+          runtime = pkgs.stdenv.mkDerivation {
+            pname = "runtime";
+            version = "0.0.0";
+            src = self;
+            nativeBuildInputs = with pkgs; [ gcc gnumake ];
+            buildPhase = ''
+              make -C runtime/c/
+            '';
+            installPhase = ''
+              mkdir -p $out/runtime/include
+              cp runtime/c/libruntime.a $out/runtime/libruntime.a
+              cp runtime/c/runtime.h $out/runtime/include/runtime.h
+            '';
+          };
+          all = pkgs.symlinkJoin {
+            pname = "all";
+            version = "0.0.0";
+            paths = [
+              self.packages.${system}.playground
+              self.packages.${system}.runtime
+            ];
+          };
+          default = self.packages.${system}.all;
         };
-      }
-    );
+      });
 }
---
+++ runtime/c/runtime.c
@@ -0,0 +1,8 @@
+#include "runtime.h"
+
+#include <stdio.h>
+
+void print_int(int n)
+{
+    printf("%d\n", n);
+}

e627fe35b4b204e6fef25abf8c9a9183bb5394d3
Louis-Mael Derval <louis-mael.derval@epita.fr>
2025-07-06 01:18:16 +0200

    chore(simplifier): handle while loops inside the simplifier

--- bin/main.ml
+++ bin/main.ml
@@ -5,7 +5,7 @@ let lexbuf = Lexing.from_channel stdin
 let base_ast = Parser.program Lexer.lex lexbuf
 let globally_scoped_ast = GlobalScoper.create_global_scope base_ast
 let scoped_ast = InnerScoper.create_inner_scopes globally_scoped_ast
-(* let simplified_ast = BlockSimplifier.simplify_blocks scoped_ast *)
+let simplified_ast = BlockSimplifier.simplify_blocks scoped_ast
 (* let explicit_ast = Expliciter.explicit_declaration simplified_ast *)
 (* let renamed_ast = Renamer.rename_var explicit_ast *)
 (* let linearized_ast = BlockLinearizer.linearize_blocks renamed_ast *)
@@ -18,10 +18,10 @@ let _ =
   Printf.printf "\n";
   Printf.printf "# Scoped AST\n";
   PrettyPrinter.pretty_print scoped_ast;
+  Printf.printf "\n";
+  Printf.printf "# Simplified AST\n";
+  PrettyPrinter.pretty_print simplified_ast;
   Printf.printf "\n"
-(* Printf.printf "# Simplified AST\n"; *)
-(* PrettyPrinter.pretty_print simplified_ast; *)
-(* Printf.printf "\n"; *)
 (* Printf.printf "# Explicit AST\n"; *)
 (* PrettyPrinter.pretty_print explicit_ast; *)
 (* Printf.printf "\n"; *)

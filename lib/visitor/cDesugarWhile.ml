2ab74a2b2210a44629443b455b8a58cb327c2b9b
louis-mael <louismaelderval@gmail.com>
2025-07-09 16:16:12 +0200

    chore(while-desugar): handle multiple while loop

--- lib/visitor/cDesugarWhile.ml
+++ lib/visitor/cDesugarWhile.ml
@@ -13,46 +13,51 @@ let invert_condition cond =
       let e2 = CAst.IntConst 0 in
       CAst.Operator (e1, op, e2)
 
-let rec desugar_program program =
+let rec desugar_program program fresh =
   match program with
-  | [] -> []
+  | [] -> ([], fresh)
   | h :: t ->
-      let h = desugar_declaration h in
-      let t = desugar_program t in
-      h :: t
+      let h, fresh = desugar_declaration h fresh in
+      let t, fresh = desugar_program t fresh in
+      (h :: t, fresh)
 
-and desugar_declaration declaration =
+and desugar_declaration declaration fresh =
   match declaration with
   | CAst.FunctionDec (id, pars, stmt, ty) ->
-      let stmt = desugar_compound_stmt stmt in
-      CAst.FunctionDec (id, pars, stmt, ty)
-  | _ -> declaration
+      let stmt, fresh = desugar_compound_stmt stmt fresh in
+      (CAst.FunctionDec (id, pars, stmt, ty), fresh)
+  | _ -> (declaration, fresh)
 
-and desugar_compound_stmt stmt =
+and desugar_compound_stmt stmt fresh =
   match stmt with
-  | [] -> []
+  | [] -> ([], fresh)
   | h :: t ->
-      let h = desugar_compound h in
-      let t = desugar_compound_stmt t in
-      merge h t
+      let h, fresh = desugar_compound h fresh in
+      let t, fresh = desugar_compound_stmt t fresh in
+      (merge h t, fresh)
 
-and desugar_compound compound =
+and desugar_compound compound fresh =
   match compound with
   | CAst.Stmt stmt ->
-      let stmt = desugar_stmt stmt in
-      List.map (fun stmt -> CAst.Stmt stmt) stmt
-  | _ -> [ compound ]
+      let stmt, fresh = desugar_stmt stmt fresh in
+      (List.map (fun stmt -> CAst.Stmt stmt) stmt, fresh)
+  | _ -> ([ compound ], fresh)
 
-and desugar_stmt stmt =
+and desugar_stmt stmt fresh =
   match stmt with
   | CAst.While (c, s) ->
       let c = invert_condition c in
-      [
-        CAst.Label "loop";
-        CAst.If (c, CAst.Compound [ CAst.Stmt (CAst.Goto "end") ], s);
-        CAst.Goto "loop";
-        CAst.Label "end";
-      ]
-  | _ -> [ stmt ]
+      let loop_label = Printf.sprintf "loop%d" fresh in
+      let end_label = Printf.sprintf "end%d" fresh in
+      ( [
+          CAst.Label loop_label;
+          CAst.If (c, CAst.Compound [ CAst.Stmt (CAst.Goto end_label) ], s);
+          CAst.Goto loop_label;
+          CAst.Label end_label;
+        ],
+        fresh + 1 )
+  | _ -> ([ stmt ], fresh)
 
-let desugar_while program = desugar_program program
+let desugar_while program =
+  let program, _ = desugar_program program 0 in
+  program

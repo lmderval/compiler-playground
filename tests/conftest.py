09643ffa9217048b8f89d907c5020ac67000f67e
louis-mael <louismaelderval@gmail.com>
2025-07-09 23:39:01 +0200

    chore(tests): setup a pytest testuite

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import glob
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.resolve()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--binaries-directory", action="store", default=BASE_DIR)
+
+
+def pytest_generate_tests(metafunc):
+    option_value = metafunc.config.option.binaries_directory
+    if "binaries_directory" in metafunc.fixturenames and option_value is not None:
+        metafunc.parametrize("binaries_directory", [option_value])
+
+    if "testcase" in metafunc.fixturenames:
+        files_dir = BASE_DIR / "files"
+        testcases = []
+        for child in glob.glob(f"{files_dir}/**/*.plgr", recursive=True):
+            test_path = child.removesuffix(".plgr")
+            test_name = test_path.removeprefix(f"{files_dir}/").replace("/", "::")
+            testcases.append({"name": test_name, "path": test_path})
+
+        metafunc.parametrize("testcase", testcases)
+
+
+def pytest_itemcollected(item):
+    item._nodeid = ""
+
+    name = item.originalname.removeprefix("test_")
+    testcase = item.callspec.params.get("testcase")
+
+    if testcase:
+        item._nodeid = f"{name}::{testcase['name']}"
---
+++ tests/test_explicit.ml
@@ -0,0 +1,10 @@
+open Visitor
+open Parse
+
+let lexbuf = Lexing.from_channel stdin
+let base_ast = Parser.program Lexer.lex lexbuf
+let globally_scoped_ast = GlobalScoper.create_global_scope base_ast
+let scoped_ast = IfScoper.create_if_scopes globally_scoped_ast
+let simplified_ast = BlockSimplifier.simplify_blocks scoped_ast
+let explicit_ast = Expliciter.explicit_declaration simplified_ast
+let () = PrettyPrinter.pretty_print explicit_ast

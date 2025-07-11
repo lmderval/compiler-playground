cc82c59fb4cc4c0aed483ddd17771c51afdb4f94
louis-mael <louismaelderval@gmail.com>
2025-07-11 23:29:38 +0200

    chore(tests): add C execution tests

--- tests/test_playground.py
+++ tests/test_playground.py
@@ -42,3 +42,93 @@ def test_renamed(binaries_directory: str, testcase: Dict[str, str]):
 
 def test_linearized(binaries_directory: str, testcase: Dict[str, str]):
     basic_test("linearized", binaries_directory, testcase)
+
+
+def test_c_program(
+    binaries_directory: str, testcase: Dict[str, str], runtime_directory: str
+):
+    test_path = testcase['name'].replace('::', '/')
+
+    binary_path = Path(binaries_directory) / "test_c_program.exe"
+    input_path = Path(f"{testcase['path']}.plgr")
+    output_path = Path(f"{testcase['path']}.out")
+    runtime_path = Path(runtime_directory)
+    include_path = runtime_path / "include"
+    tmp_path = Path("/tmp/files/")
+    obj_path = tmp_path / f"{test_path}.o"
+    out_path = tmp_path / test_path
+    libruntime_path = runtime_path / "libruntime.a"
+
+    if not obj_path.parent.exists():
+        obj_path.parent.mkdir(parents=True)
+
+    with open(input_path, mode="br") as file:
+        input = b"".join(file.readlines())
+
+    with open(output_path, mode="br") as file:
+        output = b"".join(file.readlines())
+
+    proc = Popen(
+        args=[binary_path],
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+
+    stdout, stderr = proc.communicate(input=input, timeout=10.0)
+    assert proc.returncode == 0
+    assert stderr == b""
+
+    input = stdout
+
+    compile_proc = Popen(
+        args=[
+            "gcc",
+            "-c",
+            "-std=c99",
+            f"-I{runtime_path}",
+            f"-I{include_path}",
+            "-o",
+            f"{obj_path}",
+            "-xc",
+            "-",
+        ],
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+
+    stdout, stderr = compile_proc.communicate(input=input, timeout=10.0)
+    assert proc.returncode == 0
+    assert stdout == b""
+    assert stderr == b""
+
+    link_proc = Popen(
+        args=[
+            "gcc",
+            "-o",
+            f"{out_path}",
+            f"{obj_path}",
+            f"{libruntime_path}",
+        ],
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+
+    stdout, stderr = link_proc.communicate(input=input, timeout=10.0)
+    assert proc.returncode == 0
+    assert stdout == b""
+    assert stderr == b""
+
+    execution_proc = Popen(
+        args=[f"{out_path}"],
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+
+    stdout, stderr = execution_proc.communicate(input=input, timeout=10.0)
+    assert proc.returncode == 0
+    assert stdout == output
+    assert stderr == b""
